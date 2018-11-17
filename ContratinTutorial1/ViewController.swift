9e1437504b9cc00ebfee32875a6baf3350edb890
sanjay chaurasia <sanjaykumarchaurasia@sanjays-mac-mini.local>
2018-11-17 15:49:04 +0530

    Initial Commit

---
+++ ContratinTutorial1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContratinTutorial1
+//
+//  Created by sanjay kumar chaurasia on 17/11/18.
+//  Copyright © 2018 sanjay kumar chaurasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
